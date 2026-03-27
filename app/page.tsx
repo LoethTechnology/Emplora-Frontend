93cdb411b11a9026c7f72aa34c218c59e0dd5f74
Mbonu Chinedum <cboy.chinedu@gmail.com>
2026-03-27 21:08:13 +0100

    added margin top of 160px to the header for the home page

--- app/page.tsx
+++ app/page.tsx
@@ -17,7 +17,7 @@ const Home = () => {
       {/* Adding the main div */}
       <main className="min-h-screen bg-white p-4 md:p-[30px] mb-90.5"> 
         {/* Header Div */}
-        <header className="grid justify-center content-center items-center text-center mt-30 md:mt-[100px]"> 
+        <header className="grid justify-center content-center items-center text-center mt-30 md:mt-40"> 
           <div> 
             {/* Reduced text size for mobile (text-4xl) and scaled up for desktop (text-[62px]) */}
             <h2 className="text-4xl md:text-[62px] font-bold leading-tight md:leading-[70px] text-black"> 
