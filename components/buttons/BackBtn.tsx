0b1782f043c10fce287073fac193a7f047565b8f
Ali Noel <noelams03@gmail.com>
2026-05-02 21:11:12 +0100

    fix: replace svg props with correct names

--- components/buttons/BackBtn.tsx
+++ components/buttons/BackBtn.tsx
@@ -14,9 +14,9 @@ const BackButton = ({ className }: { className?: string }) => {
         viewBox="0 0 24 24"
         fill="none"
         stroke="black"
-        stroke-width="2"
-        stroke-linecap="round"
-        stroke-linejoin="round"
+        strokeWidth="2"
+        strokeLinecap="round"
+        strokeLinejoin="round"
       >
         <line x1="19" y1="12" x2="5" y2="12" />
         <polyline points="12 19 5 12 12 5" />
--- components/buttons/FavoriteBtn.tsx
+++ components/buttons/FavoriteBtn.tsx
@@ -14,9 +14,9 @@ const FavoriteBtn = ({ className }: { className: string }) => {
         viewBox="0 0 24 24"
         fill="none"
         stroke="black"
-        stroke-width="2"
-        stroke-linecap="round"
-        stroke-linejoin="round"
+        strokeWidth="2"
+        strokeLinecap="round"
+        strokeLinejoin="round"
       >
         <path d="M20.8 4.6a5.5 5.5 0 0 0-7.8 0L12 5.6l-1-1a5.5 5.5 0 0 0-7.8 7.8l1 1L12 21l7.8-7.6 1-1a5.5 5.5 0 0 0 0-7.8z" />
       </svg>
