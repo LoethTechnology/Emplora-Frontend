45b9e4af7d309456e9c5d517be2285f6c2208898
Mbonu Chinedum <cboy.chinedu@gmail.com>
2026-05-10 11:49:58 +0100

    started working on the search company page

--- app/(public)/search-company/page.tsx
+++ app/(public)/search-company/page.tsx
@@ -1,7 +1,10 @@
 // Importing the necessary modules 
+import Link from "next/link";
+import Image from "next/image";
 import React, { Fragment } from 'react';
 import Footer from "@/components/footer/footer";
 import Navbar from "@/components/navbar/navbar";
+import searchCompanyLogo from "@/images/searchCompany/searchCompanyLogo.png";
 
 // Creating the search company component 
 const SearchCompany = () => {
@@ -11,9 +14,39 @@ const SearchCompany = () => {
             {/* Adding the navbar */}
             <Navbar />
 
+            {/* Adding the main div */}
+            <main className="h-[100vh] w-full">
+                {/* Search company hero section */}
+                <div className="flex items-center justify-center relative w-full lg:h-101">
+                    <Image
+                        src={searchCompanyLogo}
+                        alt="Search Company Hero Logo"
+                        className="absolute w-full h-full object-cover"
+                        priority
+                    />
+                    <div className="flex justify-center flex-col align-middle relative z-10 text-center px-4 sm:px-6">
+                        <div>
+                            <h1 className="sm:text-4xl md:text-[56px] lg:text-[56px] text-white font-bold mb-4"> Search Company </h1>
+                        </div>
+                        <div className="relative flex justify-center text-[white]">
+                            <Link href="/" className="hover:text-gray-300 transition-colors"> Home </Link>
+                            <span className="mx-2"> | </span>
+                            <span> Search Company </span>
+                        </div>
+                    </div>
+                </div>
 
-            <p> Search Company </p>
+                {/* Discover and explore companies div */}
+                <div className="flex justify-center text-black mt-[40px] mb-[45px]">
+                    <h2 className="text-[32px]"> Discover and explore companies </h2>
+                </div>
 
+                {/* Location, Search, and Filter div */}
+                <div>
+
+                </div>
+
+            </main>
 
             {/* Adding the footer */}
             <Footer />
