795941e77a9fe5c9a72009e68d003b8004668a37
aliapochi <54737995+aliapochi@users.noreply.github.com>
2026-09-25 13:13:50 +0100

    Refactor SignInBtn component with formatting updates

--- components/buttons/signInBtn.tsx
+++ components/buttons/signInBtn.tsx
@@ -1,17 +1,18 @@
-// Importing the necessary modules 
+// Importing the necessary modules
 import Link from 'next/link';
 import React, { Fragment } from 'react';
 import { usePathname } from 'next/navigation';
 
-// Creating the register button 
+// Creating the register button
 const SignInBtn = () => {
     const pathname = usePathname();
-    // Rendering the component 
+
+    // Rendering the component
     return(
-        <Fragment> 
+        <Fragment>
             {/* Creating the link url for signIn */}
             <Link href={`/signin?redirect=${encodeURIComponent(pathname)}`}>
-                <button className="h-[50px] text-[15px] text-[#334EAC] border-[1px] border-[#334EAC] pl-[30px] pr-[30px] rounded-[5px]"> 
+                <button className="h-[50px] text-[15px] text-[#334EAC] border-[1px] border-[#334EAC] pl-[30px] pr-[30px] rounded-[5px]">
                     Sign In
                 </button>
             </Link>
@@ -19,5 +20,5 @@ const SignInBtn = () => {
     )
 }
 
-// Exporting the register button 
-export default SignInBtn; 
+// Exporting the register button
+export default SignInBtn;
