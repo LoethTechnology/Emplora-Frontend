5b7085b2af990e61b09b13c69df935af2718f896
aliapochi <54737995+aliapochi@users.noreply.github.com>
2026-09-25 13:11:32 +0100

    Preserve current page when opening sign in

--- components/buttons/signInBtn.tsx
+++ components/buttons/signInBtn.tsx
@@ -1,14 +1,16 @@
 // Importing the necessary modules 
 import Link from 'next/link';
-import React, { Fragment } from 'react'; 
+import React, { Fragment } from 'react';
+import { usePathname } from 'next/navigation';
 
 // Creating the register button 
 const SignInBtn = () => {
+    const pathname = usePathname();
     // Rendering the component 
     return(
         <Fragment> 
             {/* Creating the link url for signIn */}
-            <Link href="/signin"> 
+            <Link href={`/signin?redirect=${encodeURIComponent(pathname)}`}>
                 <button className="h-[50px] text-[15px] text-[#334EAC] border-[1px] border-[#334EAC] pl-[30px] pr-[30px] rounded-[5px]"> 
                     Sign In
                 </button>
